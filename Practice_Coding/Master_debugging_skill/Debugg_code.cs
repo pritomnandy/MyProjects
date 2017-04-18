e38fd9cbc1d5c5f05dfe2b254d41a5f5bfd73990
pritomnandy <pritom82@yahoo.co.in>
2017-04-18 14:52:39 +0600

    Add files via upload
    Created solution

---
+++ Practice_Coding/Master_debugging_skill/Debugg_code.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master_debugging_skill
+{
+    class Debugg_code
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"My number is : {i}");
+                }
+            Console.ReadLine();
+        }
+    }
+}
